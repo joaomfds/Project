90c355b8282955806a16bed30a7f060546b2b791
Joao Santos <joaomfsantos@hotmail.com>
2016-12-19 20:52:25 +0100

    prepare for script.js creation

---
+++ HTMLInstagram/script.js
@@ -0,0 +1,4 @@
+function initializeParse(){
+  Parse.initialize("instagram5208", "instagram5208oijopiajaoijwogeij934oin83hnvASC");
+  Parse.serverURL = 'https://frozen-sierra-62859.herokuapp.com/parse/';
+}
